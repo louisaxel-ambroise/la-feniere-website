07636331c651f401cfb675964045167acd7df0df
louisaxel-ambroise <ambroise.la@gmail.com>
2016-05-05 11:51:54 +0200

    Update index page

--- Gite.WebSite/App_Start/BundleConfig.cs
+++ Gite.WebSite/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace Gite.WebSite
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/laFeniere").Include(
+                      "~/Content/App/shared.css"));
         }
     }
 }
