783cef303b80edae56be7b090e597fb6df4e30b5
Louis-Axel Ambroise <ambroise.la@gmail.com>
2016-08-17 11:55:13 +0200

    Update CreateReservationHandler

--- Gite.Domain/Handlers/Commands/CreateReservationHandler.cs
+++ Gite.Domain/Handlers/Commands/CreateReservationHandler.cs
@@ -1,49 +1,41 @@
 ﻿using System;
 using System.Linq;
+using Gite.Cqrs.Aggregates;
 using Gite.Cqrs.Commands;
 using Gite.Messaging.Commands;
-using Gite.Messaging.Events;
+using Gite.Model.Aggregates;
+using Gite.Model.Model;
 using Gite.Model.Readers;
 
 namespace Gite.Model.Handlers.Commands
 {
-    public class CreateReservationHandler : CommandHandler<CreateReservation>
+    public class CreateReservationHandler : ICommandHandler<CreateReservation>
     {
+        private readonly IAggregateManager<ReservationAggregate> _aggregateManager;
         private readonly IBookedWeekReader _bookedWeekReader;
 
-        public CreateReservationHandler(IBookedWeekReader bookedWeekReader)
+        public CreateReservationHandler(IAggregateManager<ReservationAggregate> aggregateManager, IBookedWeekReader bookedWeekReader)
         {
+            if (aggregateManager == null) throw new ArgumentNullException("aggregateManager");
+            if (bookedWeekReader == null) throw new ArgumentNullException("bookedWeekReader");
+
+            _aggregateManager = aggregateManager;
             _bookedWeekReader = bookedWeekReader;
         }
 
-        public override void Handle(CreateReservation command)
+        public void Handle(CreateReservation command)
         {
-            if (command.AdultsCount + command.ChildrenCount > 6) throw new Exception("Maximum 6 people over 2 years are allowed.");
-
             var bookedWeeks = _bookedWeekReader.QueryValids().Any(x => x.Week >= command.FirstWeek && x.Week <= command.LastWeek);
-            if (command.FirstWeek <= DateTime.Now || bookedWeeks)
-            {
-                throw new Exception("Week is past or already booked.");
-            }
+            if (command.AdultsCount + command.ChildrenCount > 6) throw new Exception("Maximum 6 people over 2 years are allowed.");
+            if (command.FirstWeek <= DateTime.Now || bookedWeeks) throw new Exception("Week is past or already booked.");
 
-            RaiseEvent(new ReservationCreated
-            {
-                AggregateId = command.AggregateId,
-                FirstWeek = command.FirstWeek,
-                LastWeek = command.LastWeek,
-                FinalPrice = command.FinalPrice,
-                OriginalPrice = command.OriginalPrice,
-                Reduction = command.Reduction,
-                Address = command.Address,
-                Mail = command.Mail,
-                Phone = command.Phone,
-                Name = command.Name,
-                AdultsCount = command.AdultsCount,
-                ChildrenCount = command.ChildrenCount,
-                BabiesCount = command.BabiesCount,
-                AnimalsCount = command.AnimalsCount,
-                AnimalsType = command.AnimalsType
-            });
+            var price = new Price{ Final = command.FinalPrice, Original = command.OriginalPrice, Reduction = command.Reduction };
+            var contact = new Contact { Address = command.Address, Mail = command.Mail, Name = command.Name, Phone = command.Phone };
+            var people = new People{ Adults = command.AdultsCount, Children = command.ChildrenCount, Babies = command.BabiesCount, Animals = command.AnimalsCount, AnimalsDescription = command.AnimalsType };
+
+            var reservation = new ReservationAggregate(command.AggregateId, command.FirstWeek, command.LastWeek, price, contact, people);
+
+            _aggregateManager.Save(reservation);
         }
     }
 }
