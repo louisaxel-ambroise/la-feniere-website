3da4db62a13eff144a13359d17fd69b2eb8f2252
Louis-Axel Ambroise <ambroise.la@gmail.com>
2016-08-19 16:29:31 +0200

    update border

--- Gite.Domain/Services/Contract/ParagraphBorder.cs
+++ Gite.Domain/Services/Contract/ParagraphBorder.cs
@@ -4,7 +4,7 @@ using iTextSharp.text.pdf;
 namespace Gite.Model.Services.Contract
 {
     public class ParagraphBorder : PdfPageEventHelper {
-        private const float Offset = 10;
+        private const float Offset = 15;
         private float _startPosition;
         private int _currentCount = 0;
 
@@ -13,16 +13,18 @@ namespace Gite.Model.Services.Contract
 
         public override void OnParagraph(PdfWriter writer, Document document, float paragraphPosition)
         {
+            if (_currentCount == 0) _startPosition = paragraphPosition;
+
             if (IsBordered) _currentCount++;
             else _currentCount = 0;
-            _startPosition = paragraphPosition;
+            
         }
 
         public override void OnParagraphEnd(PdfWriter writer, Document document, float paragraphPosition) {
             if (!IsBordered || _currentCount != ParagraphCount) return;
 
             var cb = writer.DirectContentUnder;
-            cb.Rectangle(document.Left-10, paragraphPosition + Offset, document.Right - document.Left+10, _startPosition - paragraphPosition - Offset);
+            cb.Rectangle(document.Left-10, paragraphPosition - Offset, document.Right - document.Left+10, _startPosition - paragraphPosition + Offset);
             cb.Stroke();
             _currentCount = 0;
         }
